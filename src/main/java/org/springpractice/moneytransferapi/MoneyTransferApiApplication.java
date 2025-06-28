047f7bdade76a06fa599e03a39a5351b28c57233
matchi-1 <misheruvynx@gmail.com>
2025-06-28 21:13:11 +0800

    Initial commit

---
+++ src/main/java/org/springpractice/moneytransferapi/MoneyTransferApiApplication.java
@@ -0,0 +1,13 @@
+package org.springpractice.moneytransferapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyTransferApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyTransferApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/springpractice/moneytransferapi/MoneyTransferApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.springpractice.moneytransferapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyTransferApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
