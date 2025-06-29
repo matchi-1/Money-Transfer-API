e1bc60174a84706629845e7091b46a5f145ba698
matchi-1 <misheruvynx@gmail.com>
2025-06-30 03:44:16 +0800

    added controller and service layer for User

--- src/main/java/org/springpractice/moneytransferapi/repository/TransactionRepo.java
+++ src/main/java/org/springpractice/moneytransferapi/repository/TransactionRepo.java
@@ -3,5 +3,6 @@ package org.springpractice.moneytransferapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springpractice.moneytransferapi.entity.Transaction;
 
+
 public interface TransactionRepo extends JpaRepository<Transaction,Long> {
 }
