3e8aeed3fbf83515bc00ee794c8c153248f8cad3
matchi-1 <misheruvynx@gmail.com>
2025-06-29 18:06:46 +0800

    added transactions table/class/entity connected to user entity ++ added transaction repo

---
+++ src/main/java/org/springpractice/moneytransferapi/repository/TransactionRepo.java
@@ -0,0 +1,7 @@
+package org.springpractice.moneytransferapi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springpractice.moneytransferapi.entity.Transaction;
+
+public interface TransactionRepo extends JpaRepository<Transaction,Long> {
+}
--- src/main/java/org/springpractice/moneytransferapi/repository/UserRepo.java
+++ src/main/java/org/springpractice/moneytransferapi/repository/UserRepo.java
@@ -3,5 +3,5 @@ package org.springpractice.moneytransferapi.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springpractice.moneytransferapi.entity.User;
 
-public interface UserRepo extends JpaRepository<User,Integer> {
+public interface UserRepo extends JpaRepository<User,Long> {
 }
