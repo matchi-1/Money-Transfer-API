d43f8bc51927684023b0ad20772b9d7f2d8339ea
matchi-1 <misheruvynx@gmail.com>
2025-06-30 16:37:06 +0800

    added endpoints for find by sender/receiver id in controller-- updated transaction service and repo. LAlso added validation annotations in TransactionRequest DTO

--- src/main/java/org/springpractice/moneytransferapi/controller/TransactionController.java
+++ src/main/java/org/springpractice/moneytransferapi/controller/TransactionController.java
@@ -1,16 +1,21 @@
 package org.springpractice.moneytransferapi.controller;
 
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springpractice.moneytransferapi.dto.TransactionRequest;
 import org.springpractice.moneytransferapi.entity.Transaction;
+import org.springpractice.moneytransferapi.entity.User;
 import org.springpractice.moneytransferapi.service.TransactionService;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/transactions")
+@Validated
 public class TransactionController {
     private final TransactionService transactionService;
 
@@ -19,8 +24,9 @@ public class TransactionController {
         this.transactionService = transactionService;
     }
 
+    // add transactions
     @PostMapping
-    public ResponseEntity<Transaction> addTransaction(@RequestBody TransactionRequest request) {
+    public ResponseEntity<Transaction> addTransaction(@Valid @RequestBody TransactionRequest request) {
         return ResponseEntity.ok(transactionService.transfer(
                 request.getSenderID(),
                 request.getReceiverID(),
@@ -28,9 +34,26 @@ public class TransactionController {
                 request.getDescription()));
     }
 
+    // GET all transactions
+    @GetMapping
+    public ResponseEntity<List<Transaction>> getAllTransactions() {
+        return ResponseEntity.ok(transactionService.getAllTransactions());
+    }
 
+    // GET user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<Transaction> getTransactionById(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionById(id));
+    }
 
+    @GetMapping("/sender/{id}")
+    public ResponseEntity<List<Transaction>> getBySender(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionsBySender(id));
+    }
 
-
+    @GetMapping("/receiver/{id}")
+    public ResponseEntity<List<Transaction>> getByReceiver(@PathVariable Long id) {
+        return ResponseEntity.ok(transactionService.getTransactionsByReceiver(id));
+    }
 
 }
