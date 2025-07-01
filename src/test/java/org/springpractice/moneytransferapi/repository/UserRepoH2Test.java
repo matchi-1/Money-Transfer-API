243aee214a4a557c0365285c002181ac23ac2a4f
matchi-1 <misheruvynx@gmail.com>
2025-07-02 06:51:30 +0800

    modified prev user h2 test to use h2 (was previously overidden by testcontainers)

--- src/test/java/org/springpractice/moneytransferapi/repository/UserRepoH2Test.java
+++ src/test/java/org/springpractice/moneytransferapi/repository/UserRepoH2Test.java
@@ -5,12 +5,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.springpractice.moneytransferapi.entity.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.testcontainers.containers.PostgreSQLContainer;
-import org.testcontainers.junit.jupiter.Container;
-import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.math.BigDecimal;
 
@@ -18,21 +13,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
-@Testcontainers
 public class UserRepoH2Test {
-
-    @Container
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.3")
-            .withDatabaseName("testdb")
-            .withUsername("testuser")
-            .withPassword("testpass");
-
-    @DynamicPropertySource
-    static void overrideProps(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     @Autowired
     private UserRepo userRepo;
