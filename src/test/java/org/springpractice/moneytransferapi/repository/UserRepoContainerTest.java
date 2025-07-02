ae1b858c30c4aea20ec16cdcf11cea98984a065e
matchi-1 <misheruvynx@gmail.com>
2025-07-02 23:37:39 +0800

    modified transaction controller test

--- src/test/java/org/springpractice/moneytransferapi/repository/UserRepoContainerTest.java
+++ src/test/java/org/springpractice/moneytransferapi/repository/UserRepoContainerTest.java
@@ -19,7 +19,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @ExtendWith(SpringExtension.class)
 @DataJpaTest
 @Testcontainers
-public class UserRepoIntegrationTest {
+public class UserRepoContainerTest {
 
     @Container
     static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:15.3")
