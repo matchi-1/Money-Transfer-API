cc7349e9f5b5cbbf0e3be305310974ff4df3fad5
matchi-1 <misheruvynx@gmail.com>
2025-07-02 16:14:47 +0800

    added kafka consumer / producers, transaction service gateway, in-memory registry, and wired async to transaction controller

--- src/main/java/org/springpractice/moneytransferapi/dto/TransactionRequestEvent.java
+++ src/main/java/org/springpractice/moneytransferapi/dto/TransactionRequestEvent.java
@@ -5,7 +5,13 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class TransactionRequestEvent {
     @NotBlank
     private String requestId;
