3454cd2a7b3fac9019a86fb0726adea03db69b03
matchi-1 <misheruvynx@gmail.com>
2025-07-15 10:27:08 +0800

    redis connection success test yipee

--- src/main/java/org/springpractice/moneytransferapi/service/gateway/TransactionGatewayService.java
+++ src/main/java/org/springpractice/moneytransferapi/service/gateway/TransactionGatewayService.java
@@ -1,8 +1,11 @@
 package org.springpractice.moneytransferapi.service.gateway;
 
 import java.util.concurrent.TimeoutException;
+
+import jakarta.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import org.springpractice.moneytransferapi.dto.TransactionRequestEvent;
