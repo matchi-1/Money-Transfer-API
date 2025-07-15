111bc231f4ad4b16c97a54fc4ba62658e894a678
matchi-1 <misheruvynx@gmail.com>
2025-07-15 10:27:08 +0800

    redis connection success test yipee

---
+++ src/main/java/org/springpractice/moneytransferapi/util/RedisConnectionTest.java
@@ -0,0 +1,36 @@
+package org.springpractice.moneytransferapi.util;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class RedisConnectionTest implements CommandLineRunner {
+
+    private final StringRedisTemplate redisTemplate;
+
+    public RedisConnectionTest(StringRedisTemplate redisTemplate) {
+        this.redisTemplate = redisTemplate;
+    }
+
+    @Override
+    public void run(String... args) {
+        try {
+            String key = "ping";
+            String value = "pong";
+            redisTemplate.opsForValue().set(key, value);
+            System.out.println("Successfully set key '" + key + "' in Redis.");
+
+            String retrievedValue = redisTemplate.opsForValue().get(key);
+            System.out.println("Successfully retrieved value: '" + retrievedValue + "'");
+
+            if (value.equals(retrievedValue)) {
+                System.out.println(">>>>> REDIS CONNECTION SUCCESS!!!");
+            } else {
+                System.out.println(">>>>> REDIS MISMATCHED VALUES!!!");
+            }
+        } catch (Exception e) {
+            System.err.println(">>>>> REDIS CONNECTION FAILED: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
